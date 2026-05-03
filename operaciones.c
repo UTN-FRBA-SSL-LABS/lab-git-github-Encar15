cb61f79170acc0362a870a5273a46df4b930e270
Enzo Cardoso <encardoso@frba.utn.edu.ar>
2026-05-03 18:35:36 -0300

    Subo operaciones.c

--- operaciones.c
+++ operaciones.c
@@ -2,7 +2,7 @@
 
 /* ── sumar — ya implementada, referencia de estilo ───────────────────────── */
 // cambio de suma a const para que no sean modificados posteriormente
-int sumar(const a, const b) {
+int sumar(const int a, const int b) {
     return a + b;
 }
 
